57e1ec29040b70dd29d6e7d683987b73e86d2d39
Bryan31542 <00063820@uca.edu.sv>
2022-06-15 17:04:06 -0600

    feat: character card design :lipstick:

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import "./Character.css";
 
 const Character = ({ character = [] }) => {
   return (
     <div className="character-container">
       {character.map((item, index) => (
-        <div key={index} className="character">
+        <div key={index} className="character-card">
           <div className="card">
-            <img src={item.image} alt="" />
+            <div className="card-image">
+              <img className="img" src={item.image} alt="" />
+            </div>
             <div className="card-body">
-              <h2 className="card-title">{item.name}</h2>
-              <p>
-                {item.status} - {item.species}
-              </p>
+              <div className="card-main-info">
+                <h2 className="card-title">{item.name}</h2>
+                <p className="card-status">
+                  {item.status} - {item.species}
+                </p>
+              </div>
+              <h5 className="card-location-head">Last know location:</h5>
+              <p>{item.location.name}</p>
             </div>
           </div>
         </div>
