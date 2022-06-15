3c7a7805c5e3649cb351eaa8ed5ab0780ee18bf4
Bryan31542 <00063820@uca.edu.sv>
2022-06-15 14:26:09 -0600

    feat: initializing project :tada:

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="app-nav">
+      <div className="title-bg">
+        <a className="title" href="/">
+          Rick and Morty
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
